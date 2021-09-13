f1d69a6b840e509c648e33e95e5b8ac43a8bf5a7
John Gruber <gruberjl@gmail.com>
2021-09-13 15:55:01 -0400

    added gatsby

---
+++ gatsby-node.js
@@ -0,0 +1,7 @@
+const path = require('path');
+const fs = require('fs');
+
+exports.onPostBuild = function() {
+  fs.renameSync(path.join(__dirname, 'public'), path.join(__dirname, 'docs'))
+  fs.mkdirSync(path.join(__dirname, 'public'))
+}
